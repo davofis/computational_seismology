e0a1cd87c28f05acc7d2ceff32e3adab3ca4bd43
dvargas <davofis123@gmail.com>
2016-10-05 08:33:28 +0200

    Add DG and SE code

--- 09_discontinuos_galerkin/flux_homo.py
+++ 09_discontinuos_galerkin/flux_homo.py
@@ -1,7 +1,7 @@
 """
 Created on Wed Feb 17 18:00:41 2016
 """
-from numpy import *
+import numpy as np
 
 def flux(Q, N, ne, Ap, Am):
     """
@@ -9,7 +9,7 @@ def flux(Q, N, ne, Ap, Am):
     connected elements for element i
     """
     # for every element we have 2 faces to other elements (left and right)
-    out = zeros((ne,N+1,2))
+    out = np.zeros((ne,N+1,2))
 
     # Calculate Fluxes inside domain
     for i in range(1, ne-1):
@@ -18,42 +18,11 @@ def flux(Q, N, ne, Ap, Am):
 
     # Boundaries
     # Left 
-    out[0,0,:] = Ap @ array([0,0]) + Am @ (-Q[i,0,:])
+    out[0,0,:] = Ap @ np.array([0,0]) + Am @ (-Q[i,0,:])
     out[0,N,:] = Ap @ (Q[0,N,:]) + Am @ (Q[1,0,:])
 
     # Right
     out[ne-1,0,:] = Ap @ (-Q[ne-2,N,:]) + Am @ (-Q[ne-1,0,:])
-    out[ne-1,N,:] = Ap @ (Q[ne-1,N,:]) + Am @ array([0, 0])
+    out[ne-1,N,:] = Ap @ (Q[ne-1,N,:]) + Am @ np.array([0, 0])
     
     return out
-
-
-
-##*****************************************************************************
-## FLUX TEST
-##*****************************************************************************
-#N = 2
-#ne = 4
-#Q = ones([ne, N+1, 2])
-#
-## Inialize Flux relates matrices
-#c = 10
-#rho = 2
-#vs = 3
-#Z = rho*vs
-#
-#R = array([[Z, -Z], [1, 1]])
-#Rinv = linalg.inv(R)
-#
-#Lm= array([[-c, 0], [0, 0]])
-#Lp= array([[0, 0] , [0, c]])
-#
-#Ap = R @ Lp @ Rinv
-#Am = R @ Lm @ Rinv
-#
-#Flux = flux(Q, N, ne, Ap, Am)
-#
-##*****************************************************************************
-
-
-
